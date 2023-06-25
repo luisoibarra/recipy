e0300987ae528c095c1ff155fc8b6764095b4123
Luis Ernesto Ibarra <luise98cu@gmail.com>
2023-06-25 18:35:48 -0400

    Final Version 1.0

--- st_recipy/Home.py
+++ st_recipy/Home.py
@@ -50,22 +50,12 @@ with st.spinner("Setting up the kitchen..."):
     utils.get_graph(action_ingredient_graph_path)
     loader.write("")
 
-# TODO Change some of the text to actually the features present on the application
 st.markdown("""
-Are you tired of sifting through countless recipes online, only to find that you don't have the right ingredients 
-or that the recipe doesn't suit your dietary restrictions? Or maybe you're a food blogger or nutritionist looking 
-for a way to analyze recipes and ingredients in a more efficient and systematic manner. Well, look no further!
-
-Introducing a revolutionary application that uses graph theory to analyze recipes and ingredients. This cutting-edge 
-technology allows users to input recipes and ingredients, and then generates a visual representation of the relationships 
-between them. By analyzing the graph, users can quickly identify which ingredients are essential, which can be substituted, 
-and which should be avoided altogether.
-
-But that's not all. This application also incorporates dietary restrictions and preferences, allowing users to filter out 
-ingredients that they cannot or do not want to consume. And for food bloggers and nutritionists, the graph can be used to 
-identify patterns and trends in recipe ingredients, such as which ingredients are most commonly used together or which 
-recipes are most popular among certain demographics.
-
-So whether you're a home cook looking to simplify your recipe search or a food industry professional in need of data-driven 
-insights, this application is sure to revolutionize the way you approach recipe analysis.
+Looking for a quick and easy way to find delicious recipes that match your taste buds? 
+Look no further than our amazing recipe app! With our powerful search features, you can easily 
+search for recipes by name or by the ingredients you have on hand. Plus, our app goes beyond just 
+finding recipes - we also help you discover new flavor combinations by suggesting ingredients that 
+pair well together. And if you're ever in a pinch and need to substitute an ingredient, our app has 
+got you covered with our handy substitution feature. So why wait? Prepare to start 
+cooking up a storm!
 """)
