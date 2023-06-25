b9981726da22c117e944a8ab7890c2e68163c2d1
Marcos Valdivie <58487709+mavaldivie@users.noreply.github.com>
2023-06-25 13:27:39 -0400

    Fixed Ingredient Replacement page to have only one input box

--- st_recipy/pages/5_Replace_Ingredient.py
+++ st_recipy/pages/5_Replace_Ingredient.py
@@ -16,24 +16,16 @@ action_ingredient_graph_path = st.session_state["action_ingredient_graph_path"]
 ingredient_graph = utils.get_graph(ingredient_graph_path)
 action_ingredient_graph = utils.get_graph(action_ingredient_graph_path)
 
-ingredient_query = st.text_input("Ingredient to replace")
-ingredients, tfidf, matrix = utils.get_ingredient_vectorizer(action_ingredient_graph_path)
+all_ingredients = [x for x in action_ingredient_graph if action_ingredient_graph.nodes[x]["type"] == "ingredient"]
+selected_ingredient = st.selectbox("Select your ingredient!", all_ingredients)
 
-if ingredient_query:
-    all_ingredients = recipy.return_ingredient_tfidf_given_query(ingredient_query, tfidf, matrix, ingredients)
-    selected_ingredient = st.selectbox("Select your ingredient!", all_ingredients)
-
+if selected_ingredient:
     print(selected_ingredient)
-    
-    if selected_ingredient:
-        replacements = recipy.get_ingredient_replacements(action_ingredient_graph, ingredient_graph, selected_ingredient, recipy.get_node_louvain_partition)
+    replacements = recipy.get_ingredient_replacements(action_ingredient_graph, ingredient_graph, selected_ingredient, recipy.get_node_louvain_partition)
 
-        if len(replacements) > 0:
-            st.subheader(f"Here is a list of some possible replacements for {selected_ingredient}:")
-            for e in replacements:
-                st.write(f"- {e}")
-        else :
-            st.subheader(f"We could not find a substitute for {selected_ingredient}")
-
-        
-
+    if len(replacements) > 0:
+        st.subheader(f"Here is a list of some possible replacements for {selected_ingredient}:")
+        for e in replacements:
+            st.write(f"- {e}")
+    else :
+        st.subheader(f"We could not find a substitute for {selected_ingredient}")
